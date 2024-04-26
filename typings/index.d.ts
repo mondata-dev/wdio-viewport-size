4458df443571b8c321fe3bd18e343b93e19352dd
David Poetzsch-Heffter <git@poehe.de>
2024-04-26 15:53:03 +0200

    fixed typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,15 +9,3 @@ declare namespace WebdriverIOAsync {
     setViewportSize(width: number, height: number): Promise<void>;
   }
 }
-
-declare namespace WebdriverIO {
-  interface ViewportSize {
-    width: number;
-    height: number;
-  }
-
-  interface Browser {
-    getViewportSize(): ViewportSize;
-    setViewportSize(width: number, height: number): void;
-  }
-}

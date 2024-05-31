9c52146b29421becb4723c6edb8ce8c9a586b5c8
David Poetzsch-Heffter <git@poehe.de>
2024-05-31 14:04:59 +0200

    fixed wdio typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,18 +1,15 @@
+import "webdriverio";
+
 interface ViewportSize {
   width: number;
   height: number;
 }
 
-declare module 'webdriverio' {
-  interface BrowserObject {
-    getViewportSize(): Promise<ViewportSize>;
-    setViewportSize(width: number, height: number, retries?: number): Promise<void>;
-  }
-}
-
-declare module '@wdio/sync' {
-  interface BrowserObject {
-    getViewportSize(): Promise<ViewportSize>;
-    setViewportSize(width: number, height: number, retries?: number): Promise<void>;
+declare global {
+  namespace WebdriverIO {
+    interface Browser {
+      getViewportSize(): Promise<ViewportSize>;
+      setViewportSize(width: number, height: number, retries?: number): Promise<void>;
+    }
   }
 }
